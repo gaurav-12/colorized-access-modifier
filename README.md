58a5b52b812fd8efc1ba2678b3eb68e9d4b9cf2e
Gaurav <gaurav.12apr@gmail.com>
2021-07-04 05:19:53 +0530

    Completed for Publishing

--- README.md
+++ README.md
@@ -1,70 +1,32 @@
-# colorized-access-modifier README
+# 🌈colorized-access-modifier🌈
 
-This is the README for your extension "colorized-access-modifier". After writing up a brief description, we recommend including the following sections.
+VS Code Extension to highlight access modifiers(excluding Public) of Ruby 💎 Language.
 
-## Features
+## 😎 Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Highlight the Private and Protected access modifiers to easily find/identify them in the Code.
 
-For example if there is an image subfolder under your extension project workspace:
+![In Dark Theme](images/dark_screenshot.jpg)
+![In Light Theme](images/light_screenshot.jpg)
 
-\!\[feature X\]\(images/feature-x.png\)
+## ⚙ Extension Settings
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+You can change the Text and Background Colors for both `private` and `protected` modifiers using following settings:
 
-## Requirements
+- `colorized_access_modifier.privateBackground`: Background Color for Private
+- `colorized_access_modifier.protectedBackground`: Background Color for Protected
+- `colorized_access_modifier.privateText`: Text Color of Private
+- `colorized_access_modifier.protectedText`: Text Color of Protected
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+## 😐 Known Issues
 
-## Extension Settings
+None yet, but do let me know if you find any.
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
+## 📃 Release Notes
 
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
+Initial release.
 
 -----------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+## Enjoy! 😃
--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,104 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "colorized-access-modifier" is now active!');
+	let timeout: NodeJS.Timer | undefined = undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('colorized-access-modifier.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from colorized-access-modifier!');
+	// create a decorator type that we use to decorate private
+	const privateDecorationType = vscode.window.createTextEditorDecorationType({
+		overviewRulerColor: { id: 'colorized_access_modifier.privateBackground' },
+		overviewRulerLane: vscode.OverviewRulerLane.Full,
+		backgroundColor: { id: 'colorized_access_modifier.privateBackground' },
+		color: { id: 'colorized_access_modifier.privateText' },
+		fontWeight: 'bold',
+		isWholeLine: true
 	});
 
-	context.subscriptions.push(disposable);
+	const protectedDecorationType = vscode.window.createTextEditorDecorationType({
+		overviewRulerColor: { id: 'colorized_access_modifier.protectedBackground' },
+		overviewRulerLane: vscode.OverviewRulerLane.Full,
+		backgroundColor: { id: 'colorized_access_modifier.protectedBackground' },
+		color: { id: 'colorized_access_modifier.protectedText' },
+		fontWeight: 'bold',
+		isWholeLine: true
+	});
+
+	let activeEditor = vscode.window.activeTextEditor;
+	let isRuby = activeEditor?.document.languageId === 'ruby';
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		} else if(!isRuby) {
+			activeEditor.setDecorations(privateDecorationType, []);
+			activeEditor.setDecorations(protectedDecorationType, []);
+			return;
+		}
+		const regEx = /(^[^\S\r\n]*private$)|(^[^\S\r\n]*protected$)/gm;
+		const text = activeEditor.document.getText();
+
+		const privatesMatches: vscode.DecorationOptions[] = [];
+		const protectedMatches: vscode.DecorationOptions[] = [];
+
+		let match;
+		while ((match = regEx.exec(text))) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			const decoration:vscode.DecorationOptions = { range: new vscode.Range(startPos, endPos) };
+
+			if(match[0].trim() === 'private') {
+				privatesMatches.push(decoration);
+			} else {
+				protectedMatches.push(decoration);
+			}
+		}
+		activeEditor.setDecorations(privateDecorationType, privatesMatches);
+		activeEditor.setDecorations(protectedDecorationType, protectedMatches);
+	}
+
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+
+	if (activeEditor && isRuby) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		
+		if (activeEditor) {
+			isRuby = activeEditor.document.languageId === 'ruby';
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	// Called when Language mode for the document is changed
+	// https://code.visualstudio.com/api/references/vscode-api#workspace
+	vscode.workspace.onDidOpenTextDocument(document => {
+		isRuby = document.languageId === 'ruby';
+
+		if(activeEditor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		isRuby = event.document.languageId === 'ruby';
+
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.window.onDidChangeActiveColorTheme(theme => {
+		if(activeEditor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
 }
 
 // this method is called when your extension is deactivated
